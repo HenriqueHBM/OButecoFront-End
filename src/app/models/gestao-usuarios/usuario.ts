98d95860ec9f96233257cfeb2baaec9cd3e10f9d
HenriqueHBM <henriquemadeira1@gmail.com>
2026-08-15 00:02:10 -0300

    puxando as informacoes para os modal

--- src/app/models/gestao-usuarios/usuario.ts
+++ src/app/models/gestao-usuarios/usuario.ts
@@ -7,3 +7,21 @@ export class Usuario {
     data_criacao !: string;
     status       !: boolean
 }
+
+// function constructor(
+//     id: number,
+//     nome: string,
+//     usuario: string,
+//     senha: string,
+//     cargo: string,
+//     data_criacao: string,
+//     status: boolean,
+// ) {
+//     id = id;
+//     nome = nome;
+//     usuario = usuario;
+//     senha = senha;
+//     cargo = cargo;
+//     data_criacao = data_criacao;
+//     status = status;
+// }
